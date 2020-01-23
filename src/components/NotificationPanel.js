3cf8eb386bf1823663be352b25dea8ebae506655
haris sucipto <harisssucipto@gmail.com>
2020-01-23 17:05:05 +0700

    styling notifications

--- src/components/NotificationPanel.js
+++ src/components/NotificationPanel.js
@@ -1,15 +1,16 @@
 import React from "react";
+import "./notificationPanel.css";
 
 const NotificationPanel = () => {
   return (
-    <div>
-      <div>
-        <p>
+    <div className="notificationPanel">
+      <div className="notification">
+        <p className="notification-text">
           By accessing and using this website, you acknowledge that you have
           read and understand our Cookie Policy, Privacy Policy, and our Terms
           of Service.
         </p>
-        <button>Got it</button>
+        <button className="notifcation-button">Got it</button>
       </div>
     </div>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,21 @@
 import React from "react";
+
+import "./HomePage.css";
 import Header from "../components/Header";
 import HeroShot from "../components/HeroShot";
 import HighlightsPanel from "../components/HighlightsPanel";
 import Footer from "../components/Footer";
-import "./HomePage.css";
 import NewsletterPanel from "../components/NewsletterPanel";
+import NotificationPanel from "../components/NotificationPanel";
 
 export const HomePage = () => {
   return (
     <div className="wrapper">
-      <div className="background-header">
+      <NotificationPanel />
+      <div
+        className="background-header"
+        style={{ marginTop: "8.5rem", position: "relative" }}
+      >
         <Header />
         <HeroShot />
       </div>
