745d20aa9d050b2a1c09f639aefe8c6d7b4c0a90
haris sucipto <harisssucipto@gmail.com>
2020-01-25 06:32:36 +0700

    finish

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,7 +11,7 @@ import Footer from "../components/Footer";
 import NewsletterPanel from "../components/NewsletterPanel";
 import NotificationPanel from "../components/NotificationPanel";
 
-const initialTime = 10 * 1000;
+const initialTime = 10 * 1000; // 10 minutes
 const Event1 = "scroll";
 const Event2 = "countDown";
 
